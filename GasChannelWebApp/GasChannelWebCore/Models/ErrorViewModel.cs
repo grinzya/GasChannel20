51e7cb77dfb36196c5f978fda493d25dbdfd3d1e
Григорий <grinzya@yandex.ru>
2020-10-15 19:09:26 +0500

    Начало работы над аутентификацией

---
+++ GasChannelWebApp/GasChannelWebCore/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace GasChannelWebCore.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
