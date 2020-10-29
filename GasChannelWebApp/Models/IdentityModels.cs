bd7141ab40d9917a9a085506d932a4e48c089cf3
Григорий <grinzya@yandex.ru>
2020-10-29 18:02:44 +0500

    Исправлены зависимости

--- GasChannelWebApp/Models/IdentityModels.cs
+++ GasChannelWebApp/Models/IdentityModels.cs
@@ -2,10 +2,9 @@
 using Microsoft.AspNet.Identity.EntityFramework;
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Security.Claims;
 using System.Threading.Tasks;
 using System.Web;
+using System.Security.Claims;
 
 namespace GasChannelWebApp.Models
 {
