2170691a2d2e609904e295372c66c68706ddfd31
Григорий <grinzya@yandex.ru>
2020-11-06 15:19:33 +0500

    Исправлены зависимости, работает, но криво

--- GasChannelWebApp/Controllers/InputDataVariantsController.cs
+++ GasChannelWebApp/Controllers/InputDataVariantsController.cs
@@ -62,7 +62,118 @@ namespace GasChannelWebApp.Controllers
             }
         }
 
-        ///Доделать
+        // POST: /InputDataVariants/
+        [HttpPost]
+        [MultiButton(MatchFormKey = "action", MatchFormValue = "LoadTable")]
+        public ActionResult Index(InputDataVariants inputDataVariants)
+        {
+            GasChannelDB _database = new GasChannelDB();
+
+            #region --- Ввод тестовых данных в базу данных       
+
+            int _ID_Variant_1 = _database.Variants.Where(p => p.NameVariant == "Вариант 1" && p.Owner.ID_User == _users.CurrentUser.ID_User).First().ID_Variant;
+            InputDataVariants inputDataVariants_1 = new InputDataVariants
+            {
+                ID_Variant = _ID_Variant_1,
+                Kol_prod_gorenija = 19165,
+                Ro_0 = 1.28,
+                Ro_v = 1.29,
+                H_pechi = 3.55,
+                L_pechi = 2.15,
+                T_fume = 1223,
+                T_fume_v = 1198,
+                L_vert = 1,
+                b_vert = 0.7,
+                H_vert = 3,
+                lambda_vert = 0.05,
+                L_bor_vert_rek = 11,
+                Td_rek_fume_bor = 1176,
+                Tv_bor = 293,
+                H_bor = 2.15,
+                L_bor = 6,
+                T_rek = 450,
+                L_rek = 1.4,
+                W_rek = 2.5,
+                d_trub_rek = 57,
+                T_rek_vh = 1176,
+                W0_rek = 4.8,                
+                n_trub = 14,
+                T_trub = 593,
+                d_h_diag = 8,
+                fi_s1 = 0.95,
+                fi_s2 = 1,
+                fi_d = 1.11,
+                fi_t_st = 1.06,
+                Owner = _users.CurrentUser
+            };
+            _inputDataVariants.InsertOrUpdate(inputDataVariants_1);
+            _inputDataVariants.Save();
+
+            #endregion --- Ввод тестовых данных в базу данных
+
+            return RedirectToAction("Index");
+        }
+        public ActionResult Create()
+        {
+            ViewBag.ID_Variant = new SelectList(_variants.All.Where(t => t.Owner.ID_User == _users.CurrentUser.ID_User), "ID_Variant", "NameVariant");
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(InputDataVariants inputDataVariants)
+        {
+            if (ModelState.IsValid)
+            {
+                inputDataVariants.Owner = _users.CurrentUser;
+                _inputDataVariants.InsertOrUpdate(inputDataVariants);
+                _inputDataVariants.Save();
+            }
+            return RedirectToAction("Index");
+        }
+
+        //
+        // GET: /InputDataVariants/Edit/1
+        //[Authorize(Roles = "Admin")] // К данному методу действия могут получать доступ только пользователи с ролью Admin
+        public ActionResult Edit(int id)
+        {
+            ViewBag.ID_Variant = new SelectList(_variants.All.Where(t => t.Owner.ID_User == _users.CurrentUser.ID_User), "ID_Variant", "NameVariant");
+            return View(_inputDataVariants.All.FirstOrDefault(t => t.ID_InputDataVariant == id));
+        }
+
+        //
+        // POST: /InputDataVariants/Edit/
+
+        //[Authorize(Roles = "Admin")] // К данному методу действия могут получать доступ только пользователи с ролью Admin
+        [HttpPost]
+        public ActionResult Edit(InputDataVariants inputDataVariants)
+        {
+            if (ModelState.IsValid)
+            {
+                _inputDataVariants.InsertOrUpdate(inputDataVariants);
+                _inputDataVariants.Save();
+            }
+            return RedirectToAction("Index");
+        }
+
+        //
+        // Edit: /InputDataVariants/Delete/1
+        // [Authorize(Roles = "Admin")] // К данному методу действия могут получать доступ только пользователи с ролью Admin
+        public ActionResult Delete(int id)
+        {
+            return View(_inputDataVariants.All.FirstOrDefault(t => t.ID_InputDataVariant == id));
+        }
+
+        //
+        // POST: /InputDataVariants/Delete/1
+        //[Authorize(Roles = "Admin")] // К данному методу действия могут получать доступ только пользователи с ролью Admin
+        [HttpPost]
+        [ActionName("Delete")]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            _inputDataVariants.Remove(_inputDataVariants.All.FirstOrDefault(t => t.ID_InputDataVariant == id));
+            _inputDataVariants.Save();
+            return RedirectToAction("Index");
+        }
 
     }
 }
